ccdc2e6b7097ded94859b2e5fdf9c35df08df1cf
David <64162682+dsfaccini@users.noreply.github.com>
2025-03-16 19:54:52 +0100

    format xml

--- filebundler/models/Bundle.py
+++ filebundler/models/Bundle.py
@@ -15,22 +15,23 @@ class Bundle(BaseModel):
     def check_file_items(cls, values: List[FileItem]):
         return [fi for fi in values if fi.path.exists() and not fi.is_dir]
 
+    # FUTURE TODO, not now: create XML with a library so it handles special characters and such
     @property
     def code_export(self):
         return f"""<?xml version="1.0" encoding="UTF-8"?>
 <FileBundle>
-    {"".join(make_file_section(file_item) for file_item in self.file_items if not file_item.is_dir)}</FileBundle>"""
+{"\n".join(make_file_section(file_item) for file_item in self.file_items if not file_item.is_dir)}
+</FileBundle>"""
 
 
 def make_file_section(file_item: FileItem):
     # NOTE we could add error handling
 
-    return f"""<File>
-    <FilePath>
+    return f"""    <File>
+        <FilePath>
         {file_item}
-    </FilePath>
-    <FileContent>
+        </FilePath>
+        <FileContent>
 {read_file(file_item.path)}
-    </FileContent>
-</File>
-"""
+        </FileContent>
+    </File>"""
