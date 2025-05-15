56780754368adc2ed9e2f69236acd831ca8dbb8e
David <faccini@brettschneider.com>
2025-05-15 08:11:30 -0500

    stricter typing

--- filebundler/models/FileItem.py
+++ filebundler/models/FileItem.py
@@ -11,7 +11,7 @@ class FileItem(BaseModel):
     path: Path
     project_path: Path
     parent: Optional["FileItem"] = Field(None, exclude=True)
-    children: List["FileItem"] = Field([], exclude=True, repr=False)
+    children: List["FileItem"] = Field([], exclude=True, repr=False)  # type: ignore
     selected: bool = Field(False, exclude=True)
 
     # NOTE: activate to debug unexpected selections or deselections
@@ -30,11 +30,11 @@ class FileItem(BaseModel):
         return self
 
     @field_serializer("path")
-    def serialize_path(self, path):
+    def serialize_path(self, path: Path) -> str:
         return self.relative.as_posix()
 
     @field_serializer("project_path")
-    def serialize_project_path(self, project_path):
+    def serialize_project_path(self, project_path: Path) -> str:
         return project_path.resolve().as_posix()
 
     @property
@@ -59,7 +59,7 @@ class FileItem(BaseModel):
         if self.path.is_file():
             return count_tokens(self.content)  # type: ignore
         else:
-            return sum(fi.tokens for fi in self.children)
+            return sum(fi.tokens for fi in self.children)  # type: ignore
 
     def toggle_selected(self):
         self.selected = not self.selected
